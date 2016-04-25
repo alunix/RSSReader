23751d8e3eb034026afb39a003fd0bd5507f2374
Igor Dotsenko <igor.dotsenko@railsreactor.com>
2016-04-25 12:25:38 +0300

    Navigation bug fixed

--- app/src/main/java/com/igordotsenko/dotsenkorssreader/MainActivity.java
+++ app/src/main/java/com/igordotsenko/dotsenkorssreader/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity
                 showItemListFragment();
                 return;
             }
+            mChannelListFragment.setLastSelectedChannel(new Channel());
             showChannelListFragment();
             return;
         }
